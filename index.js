76968272f78ab68dc59112fbd12c932d34efe4d4
Marco Argentieri <marco@vulpem.com>
2018-03-28 00:36:19 +0200

    solc compiler, only one contract per time

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ class testiero {
       this.privateKey = new Buffer(this.privateKey, 'hex')
       // Signs the given transaction data and sends it. Abstracts some of the details 
       // of buffering and serializing the transaction for web3.
-      const transaction = new Tx(txData).sign(this.privateKey)
+      const transaction = new Tx(txData)
+      transaction.sign(this.privateKey)
       const serializedTx = transaction.serialize().toString('hex')
       this.web3.eth.sendSignedTransaction('0x' + serializedTx)
         .then(resolve)
@@ -24,32 +25,28 @@ class testiero {
     })
   }
   
-  compile(name, file, contract) {
-    const input = { [name]: file }
+  compile(file, contract) {
+    const input = { [contract]: file }
     const compiledContract = solc.compile({ sources: input })
-    const abi = JSON.parse(compiledContract.contracts[`${name}:${contract}`].interface)
-    const bytecode = '0x' + compiledContract.contracts[`${name}:${contract}`].bytecode
+    const abi = JSON.parse(compiledContract.contracts[`${contract}:${contract}`].interface)
+    const bytecode = '0x' + compiledContract.contracts[`${contract}:${contract}`].bytecode
     return { abi, bytecode, contract }
   }
   
   
-  deploy(output, args, {}) {
+  deploy(output, args) {
     const { abi, bytecode, contract } = output
     // get the number of transactions sent so far so we can create a fresh nonce
     return new Promise((resolve, reject) =>
-      this.web3.eth.getTransactionCount(addressFrom)
-        .then(txCount => sendSigned({
+      this.web3.eth.getTransactionCount(this.deployer)
+        .then(txCount => this.sendSigned({
           nonce: this.web3.utils.toHex(Number(txCount)),
           data: bytecode,
           arguments: args,
-          gasLimit: this.web3.utils.toHex(1500000),
+          gasLimit: this.web3.utils.toHex(4500000),
           gasPrice: this.web3.utils.toHex(10e9), // 10 Gwei
-          from: deployer,
-        })).then((result) => {
-          console.log('txHash', result.transactionHash)
-          return result.contractAddress
-        })
-        .then(resolve)
+          from: this.deployer,
+        })).then((result) => resolve(result.contractAddress))
         .catch(reject)
     )
   }
