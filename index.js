8efa20cf75946e18e642c31e10de3225395fbed1
Marco Argentieri <marco@vulpem.com>
2018-04-03 16:36:27 +0200

    Fixing arguments at constructor

--- index.js
+++ index.js
@@ -7,13 +7,12 @@ class testiero {
   constructor(provider, privateKey) {
     this.web3 = new Web3(new Web3.providers.HttpProvider(provider))
     this.deployer = this.web3.eth.accounts.privateKeyToAccount('0x' + privateKey).address
-    this.privateKey = privateKey
+    this.privateKey = new Buffer(privateKey, 'hex')
   }
 
   sendSigned(txData) {
     return new Promise((resolve, reject) => {
       if (!this.privateKey) reject('Missing private key')
-      this.privateKey = new Buffer(this.privateKey, 'hex')
       // Signs the given transaction data and sends it. Abstracts some of the details 
       // of buffering and serializing the transaction for web3.
       const transaction = new Tx(txData)
@@ -24,7 +23,7 @@ class testiero {
         .catch(reject)
     })
   }
-  
+
   compile(file, contract) {
     const input = { [contract]: file }
     const compiledContract = solc.compile({ sources: input })
@@ -34,19 +33,23 @@ class testiero {
     const bytecode = '0x' + output.bytecode
     return { abi, bytecode, contract, gasEstimates }
   }
-  
-  
+
+
   deploy(output, args) {
     const { abi, bytecode, contract } = output
+
+    const instance = new this.web3.eth.Contract(abi)
+
+    const hexdata = instance.deploy({ data: bytecode, arguments: args })
+      .encodeABI()
     // get the number of transactions sent so far so we can create a fresh nonce
     return new Promise((resolve, reject) =>
       this.web3.eth.getTransactionCount(this.deployer)
         .then(txCount => this.sendSigned({
           nonce: this.web3.utils.toHex(Number(txCount)),
-          data: bytecode,
-          arguments: args,
+          data: hexdata,
           gasLimit: this.web3.utils.toHex(4500000),
-          gasPrice: this.web3.utils.toHex(10e9), // 10 Gwei
+          gasPrice: this.web3.utils.toHex(20e9), // 20 Gwei
           from: this.deployer,
         })).then(instance => this.web3.eth.getTransactionReceipt(instance.transactionHash))
         .then((receipt) => resolve(receipt))
